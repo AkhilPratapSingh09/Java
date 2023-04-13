6baa9a6c67593924af2464c28a36da2f1fc1ca09
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-04-14 01:08:31 +0530

    .

---
+++ StringPractice.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class StringPractice {
+    public static void main(String[]args){
+        String str1="Cone";
+        String str2=new String("Pyramid");
+
+        // System.out.println(str1.equalsIgnoreCase(str2));
+        // System.out.println(str1==str2);
+        // System.out.println(str1.equals(str2));
+
+        // System.out.println(str1.compareTo(str2));
+        System.out.println(str1.concat(str2));
+    }
+}
